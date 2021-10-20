d15c4cf3c29e1d049a084a71e5a458127efc133b
Romain Curtet <curt01@live.fr>
2021-10-20 18:13:32 +0200

    Résolution problèmes de Mise en page header + tableau courses

--- courses.php
+++ courses.php
@@ -1,27 +1,27 @@
 <?php
 session_start();
-if (!isset($_SESSION['isConnected']) || $_SESSION["login"] != "R&S-CURT") {
+if (!isset($_SESSION["isConnected"]) || $_SESSION["login"] != "R&S-CURT") {
     header("Location: index.php");
 }
 include("header.php");
 include("bdd.php");
-$sql = doSQL("SELECT * from courses ORDER BY importance DESC", array());
+$sql = doSQL("SELECT * from courses ORDER BY importance DESC, produit ASC", array());
 ?>
 
 <body>
-    <div class='container-list'>
-        <div class='col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 liste'>
-            <form action='sendPost.php' method='post' enctype='multipart/form-data'>
-                <input type='hidden' name='tache' value='addCourses'>
-                <table class='table table-bordered'>
+    <div class="container-list">
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 liste">
+            <form action="sendPost.php" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="tache" value="addCourses">
+                <table class="table table-bordered">
                     <tr>
-                        <input type='text' name='produit' id='produit' value=''>
+                        <input type="text" name="produit" id="produit" value="">
                         <select name="importance" id="importance">
                             <option value="Urgent">Urgent</option>
-                            <option value="Normal">Normal</option>
+                            <option value="Normal" selected>Normal</option>
                             <option value="Faible">Faible</option>
                         </select>
-                        <input type='submit' class='action' value='Enregistrer'>
+                        <input type="submit" class="action" value="Enregistrer">
                     </tr>
                 </table>
             </form>
@@ -29,60 +29,57 @@ $sql = doSQL("SELECT * from courses ORDER BY importance DESC", array());
         <div>
             <?php
             foreach ($sql as $row) {
-                echo "<div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 liste'>
-                        <table class='table table-bordered'>
+                echo '<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 liste">
+                        <table class="table table-bordered table-courses">
                             <tr>
-                            <form action='sendPost.php' method='post' enctype='multipart/form-data'>
+                            <form action="sendPost.php" method="post" enctype="multipart/form-data">
                                 <td>
-                                    <input type='text' name='produit' id='produit' value='" . $row["produit"] . "'>
+                                    <input type="text" name="produit" id="produit" value="' . $row["produit"] . '">
                                 </td>
                                 <td>
-                                    <select name='importance' id='importance'>";
-                if ($row["importance"] == 'Urgent') {
-                    echo "<option value='" . $row["importance"] . "' selected>" . $row["importance"] . "</option>
-                                                <option value='Normal'>Normal</option>
-                                                <option value='Faible'>Faible</option>
-                                                <option value='Bon'>Bon</option>";
-                } else if ($row["importance"] == 'Normal') {
-                    echo "<option value='Urgent'>Urgent</option>
-                                                <option value='" . $row["importance"] . "' selected>" . $row["importance"] .
-                        "</option>
-                                                <option value='Faible'>Faible</option>
-                                                <option value='Bon'>Bon</option>";
-                } else if ($row["importance"] == 'Faible') {
-                    echo "<option value='Urgent'>Urgent</option>
-                                                <option value='Normal'>Normal</option>
-                                                <option value='" . $row["importance"] . "' selected>" . $row["importance"] .
-                        "</option>
-                                                <option value='Bon'>Bon</option>";
-                } else if ($row["importance"] == 'Bon') {
-                    echo "<option value='Urgent'>Urgent</option>
-                                                <option value='Normal'>Normal</option>
-                                                <option value='Faible'>Faible</option>
-                                                <option value='" . $row["importance"] . "' selected>" . $row["importance"] .
-                        "</option>";
+                                    <select name="importance" id="importance">';
+                if ($row["importance"] == "Urgent") {
+                    echo '<option value="' . $row["importance"] . '" selected>' . $row["importance"] . '</option>
+                        <option value="Normal">Normal</option>
+                        <option value="Faible">Faible</option>
+                        <option value="Bon">Bon</option>';
+                } else if ($row["importance"] == "Normal") {
+                    echo '<option value="Urgent">Urgent</option>
+                        <option value="' . $row["importance"] . '" selected>' . $row["importance"] . '</option>
+                        <option value="Faible">Faible</option>
+                        <option value="Bon">Bon</option>';
+                } else if ($row["importance"] == "Faible") {
+                    echo '<option value="Urgent">Urgent</option>
+                        <option value="Normal">Normal</option>
+                        <option value="' . $row["importance"] . '" selected>' . $row["importance"] . '</option>
+                        <option value="Bon">Bon</option>';
+                } else if ($row["importance"] == "Bon") {
+                    echo '<option value="Urgent">Urgent</option>
+                        <option value="Normal">Normal</option>
+                        <option value="Faible">Faible</option>
+                        <option value="' . $row["importance"] . '" selected>' . $row["importance"] . '</option>';
                 }
-                echo "</select>
+                echo '</select>
                                 </td>
                                 <td>
-                                        <input type='hidden' name='tache' value='updateCourses'>
-                                        <input type='hidden' name='id' value='" . $row['id'] . "'>
-                                        <input type='submit' class='action' value='Modifier'>
+                                    <input type="hidden" name="tache" value="updateCourses">
+                                    <input type="hidden" name="id" value="' . $row["id"] . '">
+                                    <input type="submit" class="action" value="Modifier">
                                     
                                 </td>
                             </form>
-                            <form action='sendPost.php' method='post' enctype='multipart/form-data'>
+                            <form action="sendPost.php" method="post" enctype="multipart/form-data">
                                 <td>
-                                        <input type='hidden' name='tache' value='checkCourses'>
-                                        <input type='hidden' name='id' value='" . $row['id'] . "'>
-                                        <input type='hidden' name='produit' value='" . $row["produit"] . "'>
-                                        <input type='submit' class='btn btn-success' value='OK'>
+                                    <input type="hidden" name="tache" value="checkCourses">
+                                    <input type="hidden" name="id" value="' . $row["id"] . '">
+                                    <input type="hidden" name="produit" value="' . $row["produit"] . '">
+                                    <input type="submit" class="btn btn-success" value="OK">
                                 </td>
                             </form>
                             </tr>
                         </table>
                     <br>
-                </div>";
+                </div>';
             } ?>
         </div>
     </div>
