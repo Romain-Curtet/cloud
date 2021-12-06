f814dc2ab9c9a90b8640fdc89cb1e3a31a237c0f
Romain Curtet <curt01@live.fr>
2021-12-06 17:52:51 +0100

    Ajout de l'icone pour afficher les mots de passe

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("header.php")
+include("header.php");
 ?>
 
 <body>
@@ -8,7 +8,7 @@ include("header.php")
         <h1 class="title">Se connecter</h1>
         <?php
         if (isset($_GET["errorA"]) == true) {
-            echo '<div class="col-md-12 error"> * Ces informations sont requises</div>' . $compte;
+            echo '<div class="col-md-12 error"> * Ces informations sont requises</div>';
         } else if (isset($_GET["errorD"]) == true) {
             echo '<div class="col-md-12 error">Le login est le mot de passe ne correspondent pas</div>';
         }
@@ -24,6 +24,10 @@ include("header.php")
                     <label for="password" class="password">Mot de passe</label>
                     <input type="password" name="password" id="password" class="form-control"
                         placeholder="Votre mot de passe">
+                    <div class="password-icon">
+                        <i data-feather="eye"></i>
+                        <i data-feather="eye-off"></i>
+                    </div>
                 </div>
                 <div class="col-md-12">
                     <input type="submit" class="submit" value="Login">
@@ -47,17 +51,25 @@ include("header.php")
                 </div>
                 <div class="col-md-6">
                     <label for="email" class="email">Email</label>
-                    <input ype="email" name="email" id="email" class="form-control" placeholder="Votre email" required>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Votre email" required>
                 </div>
                 <div class="col-md-6">
                     <label for="password" class="password">Mot de passe</label>
                     <input type="password" name="password" id="password" class="form-control"
                         placeholder="Votre mot de passe" required>
+                    <div class="password-icon">
+                        <i data-feather="eye"></i>
+                        <i data-feather="eye-off"></i>
+                    </div>
                 </div>
                 <div class="col-md-6">
                     <label for="confpswd" class="confpswd">Confirmation mot de passe</label>
                     <input type="password" name="confpswd" id="confpswd" class="form-control"
                         placeholder="Confirmer votre mot de passe" required>
+                    <div class="password-icon">
+                        <i data-feather="eye"></i>
+                        <i data-feather="eye-off"></i>
+                    </div>
                 </div>
                 <div class="col-md-12">
                     <input type="submit" class="submit" value="Créer">
@@ -66,3 +78,23 @@ include("header.php")
         </form>
     </div>
     <?php include("footer.php"); ?>
+
+    <script>
+    feather.replace();
+
+    const eye = document.querySelector(".feather-eye");
+    const eyeoff = document.querySelector(".feather-eye-off");
+    const passwordField = document.querySelector("input[type=password]");
+
+    eye.addEventListener("click", () => {
+        eye.style.display = "none";
+        eyeoff.style.display = "block";
+        passwordField.type = "text";
+    });
+
+    eyeoff.addEventListener("click", () => {
+        eyeoff.style.display = "none";
+        eye.style.display = "block";
+        passwordField.type = "password";
+    });
+    </script>
