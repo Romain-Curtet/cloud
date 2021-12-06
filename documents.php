a82f50d736c622f527ae560732512597f918469c
Romain Curtet <curt01@live.fr>
2021-12-06 17:46:29 +0100

    Ajout de cookies

--- documents.php
+++ documents.php
@@ -1,5 +1,23 @@
 <?php
 session_start();
+setcookie(
+    "login",
+    $_SESSION["login"],
+    [
+        'expires' => time() + 365 * 24 * 3600,
+        'secure' => true,
+        'httponly' => true,
+    ]
+);
+setcookie(
+    "password",
+    $_SESSION["password"],
+    [
+        'expires' => time() + 365 * 24 * 3600,
+        'secure' => true,
+        'httponly' => true,
+    ]
+);
 if (!isset($_SESSION["isConnected"]) || $_SESSION["login"] != "R&S-CURT") {
     header("Location: index.php");
 }
