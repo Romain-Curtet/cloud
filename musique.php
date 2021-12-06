051e0d0ce5a1cbb1ddd53bd3d20b49d8f0ce022e
Romain Curtet <curt01@live.fr>
2021-12-06 17:50:56 +0100

    Ajout de cookies + ajout des différentes catégories de select + ajout tri et affichage selon la catégorie

--- musique.php
+++ musique.php
@@ -1,33 +1,64 @@
 <?php
 session_start();
+setcookie(
+    "login",
+    $_SESSION["login"],
+    [
+        'expires' => time() + 365 * 24 * 3600,
+        'secure' => true,
+        'httponly' => true,
+    ]
+);
+setcookie(
+    "password",
+    $_SESSION["password"],
+    [
+        'expires' => time() + 365 * 24 * 3600,
+        'secure' => true,
+        'httponly' => true,
+    ]
+);
 if (!isset($_SESSION["isConnected"]) || $_SESSION["login"] != "groupe") {
     header("Location: index.php");
 }
 include("header.php");
 include("bdd.php");
-$sql = doSQL("SELECT * from musique", array());
-$sql1 = doSQL("SELECT DISTINCT chanson from musique ORDER BY chanson ASC", array());
+$sql = doSQL("SELECT DISTINCT titre from chansons", array());
+$sql1 = doSQL("SELECT DISTINCT style from chansons", array());
+$sql2 = doSQL("SELECT DISTINCT vitesse from chansons", array());
+$sql3 = doSQL("SELECT DISTINCT difficulte from chansons", array());
 ?>
 
 <body>
     <select name="sort" id="song">
-        <option> -- </option>
+        <option>Chanson</option>
         <?php
-        foreach ($sql1 as $row1) {
-            echo '<option name="chanson" value="' . $row1["chanson"] . '">' . $row1["chanson"] . '</option>';
+        foreach ($sql as $row) {
+            echo '<option name="chanson" value="' . $row["titre"] . '">' . $row["titre"] . '</option>';
         } ?>
     </select>
+    <select name="sort" id="style">
+        <option>Style</option>
+        <?php
+        foreach ($sql1 as $row1) {
+            echo '<option name="style" value="' . $row1["style"] . '">' . $row1["style"] . '</option>';
+        } ?>
+    </select>
+    <select name="sort" id="speed">
+        <option>Vitesse</option>
+        <?php
+        foreach ($sql2 as $row2) {
+            echo '<option name="vitesse" value="' . $row2["vitesse"] . '">' . $row2["vitesse"] . '</option>';
+        } ?>
+    </select>
+    <select name="sort" id="difficult">
+        <option>Difficulté</option>
+        <?php
+        foreach ($sql3 as $row3) {
+            echo '<option name="chanson" value="' . $row3["difficulte"] . '">' . $row3["difficulte"] . '</option>';
+        }  ?>
+    </select>
     <br><br>
-    <div class="container-list">
-        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 liste">
-            <form action="sendPost.php" method="post" enctype="multipart/form-data">
-                <input type="hidden" name="tache" value="addChanson">
-                <input type="text" name="chanson" id="chanson" value="" placeholder="Titre de la chanson">
-                <input type="file" name="fichier" id="fichier" value="">
-                <input type="submit" class="action" value="Enregistrer">
-            </form>
-        </div>
-    </div>
     <div class="container-list" id="tabAjax">
         <?php
 
@@ -48,5 +79,51 @@ $sql1 = doSQL("SELECT DISTINCT chanson from musique ORDER BY chanson ASC", array
             }
         });
     }
+    console.log(song.value);
     song.onclick = ftnChanson;
+
+    function ftnDifficulte() {
+        $.ajax("sendPost.php", {
+            type: "POST",
+            data: {
+                tache: "afficherDifficulte",
+                difficulte: difficult.value,
+            },
+            success: function(resp) {
+                tabAjax.innerHTML = resp;
+            }
+        });
+    }
+    console.log(difficult.value);
+    difficult.onclick = ftnDifficulte;
+
+    function ftnStyle() {
+        $.ajax("sendPost.php", {
+            type: "POST",
+            data: {
+                tache: "afficherStyle",
+                style: style.value,
+            },
+            success: function(resp) {
+                tabAjax.innerHTML = resp;
+            }
+        });
+    }
+    console.log(style.value);
+    style.onclick = ftnStyle;
+
+    function ftnVitesse() {
+        $.ajax("sendPost.php", {
+            type: "POST",
+            data: {
+                tache: "afficherVitesse",
+                vitesse: speed.value,
+            },
+            success: function(resp) {
+                tabAjax.innerHTML = resp;
+            }
+        });
+    }
+    console.log(speed.value);
+    speed.onclick = ftnVitesse;
     </script>
