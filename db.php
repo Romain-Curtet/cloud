e881bce2ea59cb43fa6126aef2a7d3a4848b2ce6
Romain Curtet <curt01@live.fr>
2022-02-07 09:18:11 +0100

    correction mineures

--- db.php
+++ db.php
@@ -4,7 +4,6 @@ function doSQL($sql, $listParam)
 {
     try {
         $db = new PDO("mysql:host=localhost;port=3308;dbname=cloud;charset=UTF8", "root", "");
-        //$db = new PDO("mysql:host=localhost;dbname=romaincu_cloud;charset=UTF8", "romaincu_romaincu", "Curt290887.");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $request = $db->prepare($sql);
